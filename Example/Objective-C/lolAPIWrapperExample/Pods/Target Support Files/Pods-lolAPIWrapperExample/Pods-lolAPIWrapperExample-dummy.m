30f1c6a48dd47cc7b7a19b6e5e65cdc9d096c4cd
Riley Durant <rdrnt@Rileys-iMac.local>
2016-03-21 19:39:02 -0400

    Added Objective-C examples.

---
+++ Example/Objective-C/lolAPIWrapperExample/Pods/Target Support Files/Pods-lolAPIWrapperExample/Pods-lolAPIWrapperExample-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_lolAPIWrapperExample : NSObject
+@end
+@implementation PodsDummy_Pods_lolAPIWrapperExample
+@end
---
+++ Example/Objective-C/lolAPIWrapperExample/lolAPIWrapperExample/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  lolAPIWrapperExample
+//
+//  Created by Riley on 2016-03-21.
+//  Copyright © 2016 rdurant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@import lolAPIWrapper;
+
+
+@interface ViewController : UIViewController
+
+
+@end
+
